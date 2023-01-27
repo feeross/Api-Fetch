90c2e0fa185fa77a224157dd44118e5c6375d975
Fernanda Rosales <fernanda.rosales.ortega@gmail.com>
2023-01-26 22:20:26 -0600

    correcion tiempo

--- api.js
+++ api.js
@@ -1,61 +1,41 @@
 console.log(" Fetch API");
 
-const getUsers = ( ) => {
-  const url = `https://reqres.in/api/users?delay=3` ;
+const getUsers = (url = "https://reqres.in/api/users?delay=3") => {
+  let caducidadFecha = localStorage.getItem("fecha-termino");
+  if (Object.is(null, caducidadFecha) || (new Date().getTime() > caducidadFecha)) {
 
     fetch(url)
-        .then( response=> {
-            console.log("Status: " + response.status);
-            return response.json();
-        })
-        .then( users => {
-            console.log("Total de elementos: " +  users.total  );
-            //Leer a Data = [ {},{},{} ]
-            //console.log( users.data );
-            localStorage.setItem("users", JSON.stringify(users)  );
-            localStorage.setItem("fecha-actual", (new Date().getTime()) );
-            for ( let user of users.data)
-            document.getElementById('data').innerHTML += 
-            `<tr>
-            <td id="id">${user.id}</td>
-              <td id="firstName">${user.first_name}</td>
-              <td id="lastName">${user.last_name}</td>
-              <td id="email">${user.email}</td>
-              <td><img id="avatar" src="${user.avatar}"></td>
-              </tr>`             
+      .then(response => { return response.json() })
+      .then(users => {
+        localStorage.setItem("savedUsers", JSON.stringify(users.data));
+        localStorage.setItem("fecha-termino", (new Date().getTime()) + 60000);
+        imprimirDatos();
+      })
 
-        })
-        .catch( error => console.log(error));
-}
-
-
-function readLocalStorage(){
-    console.log( JSON.parse(localStorage.getItem('users'))   );
-}
-
-//almacenar datos de fecha
-localStorage.setItem("fecha-caducidad", (new Date().getTime()) + 60_000 );
-var fechaCaducidad = parseInt(localStorage.getItem("fecha-caducidad"));
-//console.log(fechaCaducidad);
-
-localStorage.setItem("fecha-actual", (new Date().getTime()) );
-var fechaActual = parseInt(localStorage.getItem("fecha-actual"));
-//console.log(fechaActual);
-
-var fechaFinal = fechaCaducidad - fechaActual;
-//console.log(fechaFinal);
-
-//borrar datos de localSotrage
-function borrarLocalStorage () {
-  if (fechaCaducidad - fechaActual >= 6000) {
-    localStorage.removeItem("users"); 
+      .catch(error => {
+        console.log(error);
+      })
   }
-  
+  else {
+    imprimirDatos();
+  }
 }
-
-
-
-
+function imprimirDatos() {
+  const users = JSON.parse(localStorage.getItem("savedUsers"));
+  console.log(users)
+  const table = document.getElementById('data');
+  table.innerHTML = "";
+  for (let index = 0; index < users.length; index++) {
+    table.innerHTML +=
+      `<tr>
+                <td id="id">${users[index].id}</td>
+                  <td id="firstName">${users[index].first_name}</td>
+                  <td id="lastName">${users[index].last_name}</td>
+                  <td id="email">${users[index].email}</td>
+                  <td><img id="avatar" src="${users[index].avatar}"></td>
+                  </tr>`
+  }
+}
 
 
 
